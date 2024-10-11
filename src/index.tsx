4bdf75f04cfa5500c0d94dd51a119e28c091bcdc
Galina Bobrik <galagrinko@gmail.com>
2024-10-11 14:49:19 +0400

    components and style.modules added

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
-import { ToDoList } from './pages/ToDoList';
+import './assets/scss/normalize.css';
+import './assets/scss/style.scss';
+import { ToDoListPage } from './pages/ToDoListPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <ToDoList />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ToDoListPage />
+    </React.StrictMode>,
 );
