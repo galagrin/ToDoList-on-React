31a1ef8c58965ec2bcda60db2e08d1f3d71ac6a4
Galina Bobrik <galagrinko@gmail.com>
2024-10-13 11:38:49 +0400

    react-toastify added

--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -3,6 +3,8 @@ import { Form } from '../components/Form/Form';
 import { Header } from '../components/Header/Header';
 import { ToDoList } from '../components/ToDoList/ToDoList';
 import { ToDo } from '../models/todo-item';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const ToDoListPage = () => {
     const [todos, setTodos] = useState<ToDo[]>([]);
@@ -13,6 +15,7 @@ export const ToDoListPage = () => {
             text: text,
             isDone: false,
         };
+        toast('Задача создана');
         setTodos([...todos, newTodo]);
     };
 
@@ -23,11 +26,13 @@ export const ToDoListPage = () => {
             }
             return todo;
         });
+        toast('Статус задачи изменен');
         setTodos(newToDos);
     };
 
     const deleteToDo = (toDoItem: ToDo) => {
         const newToDos = todos.filter((todo) => todo.id !== toDoItem.id);
+        toast('Задача удалена');
         setTodos(newToDos);
     };
 
@@ -36,6 +41,7 @@ export const ToDoListPage = () => {
             <Header />
             <Form createNewToDo={createNewToDo} />
             <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo} />
+            <ToastContainer position="bottom-right" />
         </>
     );
 };
