65e49bc8453e77d731ca5d15678b337bf4dca0bb
Galina Bobrik <galagrinko@gmail.com>
2024-10-13 13:28:05 +0400

    routing added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,20 @@
+import { NavLink } from 'react-router-dom';
 import classes from './Header.module.scss';
 
 export const Header = () => {
+    const getActiveClass = ({ isActive }: { isActive: boolean }): string => {
+        return isActive ? `${classes.active} ${classes.link}` : classes.link;
+    };
     return (
         <header className={classes.header}>
             <div className={classes.container}>
-                <a href="/" className={classes.link}>
+                <NavLink to="/" className={getActiveClass}>
+                    Home
+                </NavLink>
+
+                <NavLink to="/todo" className={getActiveClass}>
                     ToDo
-                </a>
+                </NavLink>
             </div>
         </header>
     );
--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Form } from '../components/Form/Form';
-import { Header } from '../components/Header/Header';
+
 import { ToDoList } from '../components/ToDoList/ToDoList';
 import { ToDo } from '../models/todo-item';
 import { ToastContainer, toast } from 'react-toastify';
@@ -38,7 +38,6 @@ export const ToDoListPage = () => {
 
     return (
         <>
-            <Header />
             <Form createNewToDo={createNewToDo} />
             <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo} />
             <ToastContainer position="bottom-right" />
